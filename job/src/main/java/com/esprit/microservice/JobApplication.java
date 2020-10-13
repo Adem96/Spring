33e392c6577dd417cd072df9fcf3fba66fe8347f
Adem24 <adem.benzarb@esprit.tn>
2020-10-13 10:59:44 +0100

    adem

---
+++ job/src/main/java/com/esprit/microservice/JobApplication.java
@@ -0,0 +1,27 @@
+package com.esprit.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.stream.Stream;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JobApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(JobApplication.class, args);
+		}
+		@Bean
+		ApplicationRunner init (JobRepository repository) {
+			return args ->{
+				repository.save(new Job("aaaa",true));
+				repository.save(new Job("bbbbb",false));
+				repository.findAll().forEach(System.out::println);
+			};
+		}
+	
+
+}
